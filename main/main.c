9f413aec0c4f844c1eea0de6cfbd8b756a2f40e0
aspcodenet <stefan.holmberg@systementor.se>
2024-01-26 12:32:05 +0100

    aaaa

--- main/main.c
+++ main/main.c
@@ -2,14 +2,28 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "connect_wifi.h"
+
 // https://wokwi.com/projects/305566932847821378
 #define LED_PIN 2
 // skapa en funktion som körs på annan TASK
 // 2. Fixa en WIFI-koppling
+void check_update_task(void *pvParameter) {
+    while(1) {
+        char buf[255];
+        sprintf(buf, "%s?token=%d",UPDATE_JSON_URL,cnt);    
+    }
+}
+
+
 void app_main(void)
 {
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    connect_wifi();
+
+    xTaskCreate(sendTask, "sendToThingSpeak", 8192, NULL, 5, NULL);
 
     while(1){
         gpio_set_level(LED_PIN,1 );
