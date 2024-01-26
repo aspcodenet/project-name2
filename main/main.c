71397da7cd9eb57f447ab781faa21558253a1bc1
aspcodenet <stefan.holmberg@systementor.se>
2024-01-26 12:52:10 +0100

    aaa

--- main/main.c
+++ main/main.c
@@ -3,15 +3,71 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "connect_wifi.h"
+#include "esp_http_client.h"
 
 // https://wokwi.com/projects/305566932847821378
 #define LED_PIN 2
 // skapa en funktion som körs på annan TASK
 // 2. Fixa en WIFI-koppling
-void check_update_task(void *pvParameter) {
+
+
+// receive buffer
+char rcv_buffer[200];
+
+// esp_http_client event handler
+esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
+    
+	switch(evt->event_id) {
+        case HTTP_EVENT_REDIRECT:
+        case HTTP_EVENT_ERROR:
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            break;
+        case HTTP_EVENT_ON_DATA:
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+				strncpy(rcv_buffer, (char*)evt->data, evt->data_len);
+            }
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            break;
+    }
+    return ESP_OK;
+}
+
+
+
+
+
+// Stefans API nyckel RQZXY61HSJURBPZL
+void sendTask(void *pvParameter) {
+    int cnt = 12; // MÄTVÄRDEN FRÅN SENSORER
+    int cnt2 = 13;// MÄTVÄRDEN FRÅN SENSORER
     while(1) {
         char buf[255];
-        sprintf(buf, "%s?token=%d",UPDATE_JSON_URL,cnt);    
+        sprintf(buf, "https://api.thingspeak.com/update?api_key=RQZXY61HSJURBPZL&field1=%d&field2=%d",cnt,cnt2);    
+
+        cnt++;
+        cnt2++;
+
+        // configure the esp_http_client
+		esp_http_client_config_t config = {
+            .url = buf,
+            .event_handler = _http_event_handler,
+            .keep_alive_enable = true,        
+            .timeout_ms = 30000,
+		};
+		esp_http_client_handle_t client = esp_http_client_init(&config);
+	
+		// downloading the json file
+		esp_err_t err = esp_http_client_perform(client);        
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);        
     }
 }
 
