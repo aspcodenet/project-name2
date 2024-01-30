ca86fc3c9be19b554006ef6fe79477682c26cff3
aspcodenet <stefan.holmberg@systementor.se>
2024-01-30 10:23:56 +0100

    e

--- main/main.c
+++ main/main.c
@@ -31,21 +31,10 @@
 // skapa en funktion som körs på annan TASK
 // 2. Fixa en WIFI-koppling
 
-#define FIRMWARE_VERSION	0.7
-#define UPDATE_JSON_URL		"https://github.com/aspcodenet/project-name2/raw/main/bin/firmware.json"
+#define FIRMWARE_VERSION	0.1
+#define UPDATE_JSON_URL		"https://raw.githubusercontent.com/aspcodenet/project-name2/main/bin/firmware.json"
 
 
-
-void updateTask(){
-    // Fråga github finns ny version?
-    // 1. fetch https:// firmware.json from Github 
-    // 2. läsa i firmware.json finns ny version?
-    // om ja 
-    // ladda ner
-    // installera på annan partition
-    // reboot
-    esp_restart();
-}
 
 // receive buffer
 char rcv_buffer[200];
@@ -80,32 +69,78 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
 
 
 
-// Stefans API nyckel RQZXY61HSJURBPZL
-void sendTask(void *pvParameter) {
-    int cnt = 12; // MÄTVÄRDEN FRÅN SENSORER
-    int cnt2 = 13;// MÄTVÄRDEN FRÅN SENSORER
-    while(1) {
+
+void check_update_task(void *pvParameter) {
+	int cnt = 0;
+	while(1) {
         char buf[255];
-        sprintf(buf, "https://api.thingspeak.com/update?api_key=RQZXY61HSJURBPZL&field1=%d&field2=%d",cnt,cnt2);    
-
+        sprintf(buf, "%s?token=%d",UPDATE_JSON_URL,cnt);
         cnt++;
-        cnt2++;
-
-        // configure the esp_http_client
+		printf("Looking for a new firmware at %s", buf);
+	
+		// configure the esp_http_client
 		esp_http_client_config_t config = {
-            .url = buf,
-            .event_handler = _http_event_handler,
-            .keep_alive_enable = true,        
-            .timeout_ms = 30000,
+        .url = buf,
+        .event_handler = _http_event_handler,
+        .keep_alive_enable = true,        
+        .timeout_ms = 30000,
 		};
 		esp_http_client_handle_t client = esp_http_client_init(&config);
 	
 		// downloading the json file
-		esp_err_t err = esp_http_client_perform(client);        
-
-        vTaskDelay(10000 / portTICK_PERIOD_MS);        
+		esp_err_t err = esp_http_client_perform(client);
+		if(err == ESP_OK) {
+			
+			// parse the json file	
+			cJSON *json = cJSON_Parse(rcv_buffer);
+			if(json == NULL) printf("downloaded file is not a valid json, aborting...\n");
+			else {	
+				cJSON *version = cJSON_GetObjectItemCaseSensitive(json, "version");
+				cJSON *file = cJSON_GetObjectItemCaseSensitive(json, "file");
+				
+				// check the version
+				if(!cJSON_IsNumber(version)) printf("unable to read new version, aborting...\n");
+				else {
+					
+					double new_version = version->valuedouble;
+					if(new_version > FIRMWARE_VERSION) {
+						
+						printf("current firmware version (%.1f) is lower than the available one (%.1f), upgrading...\n", FIRMWARE_VERSION, new_version);
+						if(cJSON_IsString(file) && (file->valuestring != NULL)) {
+							printf("downloading and installing new firmware (%s)...\n", file->valuestring);
+							
+							esp_http_client_config_t ota_client_config = {
+								.url = file->valuestring,
+                                .keep_alive_enable = true,
+							};
+                            esp_https_ota_config_t ota_config = {
+                                    .http_config = &ota_client_config,
+                            };                            
+							esp_err_t ret = esp_https_ota(&ota_config);
+							if (ret == ESP_OK) {
+								printf("OTA OK, restarting...\n");
+								esp_restart();
+							} else {
+								printf("OTA failed...\n");
+							}
+						}
+						else printf("unable to read the new file name, aborting...\n");
+					}
+					else printf("current firmware version (%.1f) is greater or equal to the available one (%.1f), nothing to do...\n", FIRMWARE_VERSION, new_version);
+				}
+			}
+		}
+		else printf("unable to download the json file, aborting...\n");
+		
+		// cleanup
+		esp_http_client_cleanup(client);
+		
+		printf("\n");
+        vTaskDelay(30000 / portTICK_PERIOD_MS);
     }
 }
+
+
 
 
 void app_main(void)
@@ -124,7 +159,7 @@ void app_main(void)
     connect_wifi();
     
 
-    xTaskCreate(sendTask, "sendToThingSpeak", 8192, NULL, 5, NULL);
+    xTaskCreate(check_update_task, "sendToThingSpeak", 8192, NULL, 5, NULL);
 
     while(1){
         gpio_set_level(LED_PIN,1 );
